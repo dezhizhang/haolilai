107493b3aeffe7e3d9ca6346c19afaf15177b9f9
dezhizhang <1541609448@qq.com>
2019-02-17 21:24:57 +0800

    商品详情

--- pages/product/product.js
+++ pages/product/product.js
@@ -8,7 +8,8 @@ Page({
   
   // 页面的初始数据
   data: {
-
+    array: ['1', '2', '3', '4','5','6','7','8','9','10'],
+    index:0,
   },
   // 生命周期函数--监听页面加载
   onLoad: function (options) {
@@ -23,9 +24,16 @@ Page({
   loadData:function(id) {
     product.getDetailInfo(id,(res) => {
       console.log(res);
-      
+
     });
 
+  },
+
+  //选择器的设置
+  bindPickerChange:function(ev){
+    this.setData({
+      index:ev.detail.value
+    })
   }
 
  
