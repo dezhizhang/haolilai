5e4c0bd61effb63691c7d34e872622e05b6c33c4
dezhizhang <1541609448@qq.com>
2019-02-19 22:27:49 +0800

    编写购物车

---
+++ pages/cart/cart-model.js
@@ -0,0 +1,66 @@
+import { Api } from '../../util/api';
+
+const api = new Api();
+
+class Cart {
+    constructor() {
+        this.storageKeyName = 'cart';
+
+    }
+
+    add(item,counts) {
+        let cartData = this.getCartDataFromLocal();
+
+        let isHasInfo = this.isHasThatOne(item.id,cartData);
+
+        if(isHasInfo.index == -1) {
+            item.counts = counts;
+            item.selectStatus = true;
+            cartData.push(item);
+
+        } else {
+            cartData[isHasInfo.index].counts += counts;
+
+        }
+
+        wx.setStorageSync(this.storageKeyName,cartData);
+
+
+    }
+
+    getCartDataFromLocal() {
+        let res = wx.getStorageSync();
+        if(!res) {
+            res = [];
+
+        }
+            return res;
+            
+    }
+
+    isHasThatOne(id,arr) {
+       let item = null;
+       let result = { index: -1 };
+
+       for(let i=0;i<arr.length;i++){
+           item = arr[i];
+           if(item.id == id) {
+               result = {
+                   index:i,
+                   ...item
+               }
+            //    result = {
+            //        index:1,
+            //        data:item
+            //    }
+            //    break
+           }
+       }
+       return result;
+
+    }
+    
+}
+
+
+export { Cart } 
